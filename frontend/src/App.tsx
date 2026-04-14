7d6ac4466bbc2629bffb70fd92e1bbcfaf6e5e9a
Alex Russell <128105099+AlexVRussell@users.noreply.github.com>
2026-04-14 16:41:08 -0300

    feat: added team building logic, will be made into own file later in development.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,30 @@ function App() {
       .then(data => setPokemon(data.results))
   }, [])
 
+  // teamBuilder.tsx 
+  const [team, setTeam] = useState<Pokemon[]>([])
+
+  function addToTeam(poke: Pokemon) {
+    if (team.length >= 6 ) {
+      return;
+    }
+
+    const alreadyInTeam = team.find((member) => member.name === poke.name)
+
+    if(alreadyInTeam) {
+      console.log('${poke.name} is already added');
+      return;
+    }
+
+    setTeam([...team, poke])
+  }
+
+  function removeFromTeam(poke: Pokemon){
+    setTeam(team.filter((member) => member.name !== poke.name))
+  }
+
+  // end of teamBuilder.tsx
+
   return (
     <div>
       <h1>MetaDex - Your personal Competitive Pokemon ChatBot Coach</h1>
@@ -18,6 +42,15 @@ function App() {
         <div key={poke.name}>
           <p key={poke.name}>{poke.name}</p>
           <p key={poke.url}>{poke.url}</p>
+          <button onClick={() => addToTeam(poke)}>Add to Team</button>
+        </div>
+      ))}
+
+      <h2> Your Team </h2>
+      {team.map((poke) => (
+        <div key={poke.name}>
+          <p key={poke.name}>{poke.name}</p>
+          <button onClick={() => removeFromTeam(poke)}>Remove from Team</button>
         </div>
       ))}
     </div>
