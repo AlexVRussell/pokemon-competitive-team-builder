8119068dacbb0c9cbeaa794a2dfe64436346046f
Alex Russell <128105099+AlexVRussell@users.noreply.github.com>
2026-04-14 12:51:55 -0300

    feat: fetches pokemon's name and url from the PokemonAPI endpoint and displays.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+import type { Pokemon } from "./types/pokemon"
+
 function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+      .then(response => response.json())
+      .then(data => setPokemon(data.results))
+  }, [])
+
   return (
     <div>
       <h1>MetaDex - Your personal Competitive Pokemon ChatBot Coach</h1>
+
+      {pokemon.map((poke) => (
+        <div key={poke.name}>
+          <p key={poke.name}>{poke.name}</p>
+          <p key={poke.url}>{poke.url}</p>
+        </div>
+      ))}
     </div>
   )
 }
--- frontend/src/types/pokemon.ts
+++ frontend/src/types/pokemon.ts
@@ -1,4 +1,4 @@
-export interface pokemon {
+export interface Pokemon {
     name: string;
     url: string;
 }
